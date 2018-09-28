f26c9ab91722d899c70f633be4992c8c98c1400b
hanjh04 <jangho21@naver.com>
2018-09-28 16:46:31 +0900

    component 사용 연습

---
+++ propTest/src/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <div id="app">
+    <!-- <TodoHeader></TodoHeader>
+    <TodoInput v-on:addTodo="addTodo"></TodoInput>
+    <TodoList v-bind:propsdata="todoItems" @removeTodo="removeTodo"></TodoList>
+    <TodoFooter v-on:removeAll="clearAll"></TodoFooter> -->
+    <SearchContactTemplate></SearchContactTemplate>
+  </div>
+</template>
