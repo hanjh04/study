1bb61fd9707f231a29a2e7bda83e692dadb7c4c7
hanjh04 <jangho21@naver.com>
2018-10-01 19:06:09 +0900

    진행중

--- propTest/src/App.vue
+++ propTest/src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="app">
-    <search-contact></search-contact>
-    <search-component placeholder="두 글자 이상 입력 후 엔터!" v-on:search="searchEvent"></search-component>
-    <contact-list v-bind:contacts="contactlist"></contact-list>
+    <SearchContact></SearchContact>
+    <SearchComponent placeholder="두 글자 이상 입력 후 엔터!" v-on:search="searchEvent"></SearchComponent>
+    <ContactList v-bind:contacts="contactlist"></ContactList>
   </div>
 </template>
 
@@ -10,13 +10,22 @@
 import SearchContact from "./components/SearchContact.vue"
 import Search from "./components/Search.vue"
 import ContactList from "./components/ContactList.vue"
-
+Search.methods.searCh
 export default {
     name: "App",
     components: {
-        'search-contact':SearchContact,
-        'search-component':Search,
-        'contact-list':ContactList
+        'SearchContact':SearchContact,
+        'SearchComponent':Search,
+        'ContactList':ContactList
+    },
+    methods:{
+      searchEvent: function(name) {
+            if (name == '') {
+                this.contactlist = [];
+            } else {
+                this.fetchContacts(name);
+            }
+        },
     }
 }
 </script>
