1799030b85d4463f43cc5e873369319c6fd15896
hanjh04 <jangho21@naver.com>
2018-10-24 17:35:00 +0900

    vue 실습

---
+++ vue-advanced-master/vue-news/src/views/JobsView.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        
+    </div>
+</template>
+
+<script>
+import {fetchJobs} from '../api/index.js';
+
+export default {
+    created(){
+        fetchJobs()
+        .then(data => console.log(data))
+        .catch(error => console.log(error))
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ vue-advanced-master/vue-news/src/views/NewsView.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <button @click="fetchData">fetch data</button>
+    </div>
+</template>
+
+<script>
+// <button v-on:click="fetchdata"></button>
+// <button @click="fetchdata"></button>
+import { fetchNews } from '../api/index.js'
+export default {
+    methods:{
+        fetchData(){
+            fetchNews()
+            .then(data => console.log(data))
+            .then(function(data){
+
+            })
+            .then((data) => {
+
+            })
+            .then(data => {
+
+            })
+            .then(data => data)// 한줄로 줄인다 => return을 의미.
+            .catch(error => console.log(error));
+
+
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
