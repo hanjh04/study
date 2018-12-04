8bfd78840799b36bd7c5152bb0b886b246b27812
hanjh04 <jangho21@naver.com>
2018-12-05 00:42:10 +0900

    Post 보여주는 component 구조 변경

---
+++ toy/my-web/src/components/PostContent.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <ul>
+            <li v-for="data in fetchedPosts" :key="data.idx">
+                <div class="title">
+                    {{data.title}}
+                </div>
+                <div class="content">
+                    {{data.content}}
+                </div>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex';
+export default {
+    computed:{
+        ...mapGetters(['fetchedPosts'])
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ toy/my-web/src/components/PostList.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <ul>
+            <li v-for="data in fetchedPosts" :key="data.idx">
+                <router-link :to="'/posts/'+data.idx">
+                    <div class="title">
+                        {{data.title}}
+                    </div>
+                    <div class="content">
+                        {{data.content}}
+                    </div>
+                </router-link>
+            </li>
+        </ul>
+        <router-view/>
+    </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex';
+export default {
+    computed:{
+        ...mapGetters(['fetchedPosts'])
+    }
+}
+</script>
+
+<style>
+
+</style>
