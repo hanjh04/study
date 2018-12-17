6da052822fa5e3c111e07efd8a1d2a6402f5089b
hanjh04 <jangho21@naver.com>
2018-12-18 07:34:21 +0900

    오류 수정, footer 디자인 변경

--- toy/my-web/src/components/Welcome.vue
+++ toy/my-web/src/components/Welcome.vue
@@ -6,12 +6,12 @@
 </template>
 
 <script>
-// export default {
-//   name: 'welcome',
-//   props: {
-//     msg: String
-//   }
-// }
+export default {
+  name: 'welcome',
+  props: {
+    msg: String
+  }
+}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
